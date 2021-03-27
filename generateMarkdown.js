4829d52da651bf18e7c329b4b923d223ed488ff9
Lauren Kruse <laurenjmkruse@gmail.com>
2021-03-27 01:07:32 -0400

    Created function to add license badge, if applicable

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    let licenseBadge = `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    return licenseBadge;
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
