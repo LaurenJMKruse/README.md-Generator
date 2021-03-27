f943ee6ad81f3775a475a626575c8fae1a208ba0
Lauren Kruse <laurenjmkruse@gmail.com>
2021-03-26 23:56:33 -0400

    Created function to create filepaths

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let currentWorkingDirectory = process.cwd();
+    console.log(`The current working directory is: ${currentWorkingDirectory}`);
+    let generateMarkdown = fileName;
+    let userInput = data;
+    let sourceFiles = fs.writeFileSync(path.join(currentWorkingDirectory, generateMarkdown), userInput);
+    console.log(`sourceFiles is: ${sourceFiles}`);
+    return sourceFiles;
+};
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
